d660996a33b6bb5bfc02db0b7c81b6012fc4dfc6
Joaquim Verges <joaquim.verges@gmail.com>
2023-10-21 14:59:36 -0700

    update factory

--- const.ts
+++ const.ts
@@ -1,4 +1,4 @@
 export const factoryAddress: string =
-  "0xee7675F5AB72CE52730Dd64f662Bf9226112455a";
+  "0xA4a0b37823a19541D0d2e049cC935E6398b5AB9F";
 export const editionDropAddress: string =
   "0x3F01B89cef5257CCD0f37F631a40365Fb0F1886a";
